dc10899d1d1024a08de715364d22f81ab9d6b58e
Moussa Sago Sidibe <moussa.sidibe@odc.local>
2021-10-29 15:23:43 +0000

    commentaire

--- LogApp1/src/app/pages/search/search.page.ts
+++ LogApp1/src/app/pages/search/search.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/user.service';
 
 @Component({
@@ -11,7 +13,13 @@ export class SearchPage implements OnInit {
   term;
   users: any;
 
-  constructor(private user: UserService) { }
+  constructor(private user: UserService, private auth: AngularFireAuth, private route: Router) { 
+    this.auth.authState.subscribe(auth =>{
+      if(!auth){
+        this.route.navigate(['login']);
+      }
+    })
+  }
 
   ngOnInit() {
     this.user.getUsers().subscribe(users => {
